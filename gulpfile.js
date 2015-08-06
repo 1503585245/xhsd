1fe53304c2da4a1786d713b995fa649dcd29f478
hinata77 <hinatashuishui@vip.qq.com>
2015-08-06 23:15:03 +0800

    a

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * Created by hina on 8/6/15.
+ */
+var gulp = require('gulp');
+var rjs = require('gulp-requirejs');
+var stylus = require('gulp-stylus');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('index',function(){
+  rjs({
+    baseUrl : 'static/js',
+    out : 'index.js',
+    paths : {
+      jquery : './lib/jquery',
+      tabview : './widget/tabview',
+      checkLogin : './core/checklogin',
+      slider : './widget/slider'
+    },
+    include : [
+      'jquery',
+      'tabview',
+      'checkLogin',
+      'slider',
+      './page/index.js'
+    ]
+  }).pipe(gulp.dest('static/dist/js/'));
+});
+
+gulp.task('styl',function(){
+  gulp.src('./static/css/page/*.styl').pipe(stylus({
+    compress: true
+  })).pipe(gulp.dest('./static/dist/css'));
+});
